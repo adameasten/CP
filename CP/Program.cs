e4875939f701081470aa0c1f74cbc970e9cad353
Adam <adaost0110@hotmail.com>
2018-11-02 16:01:59 +0100

    Added Best movies ever text

--- CP/Program.cs
+++ CP/Program.cs
@@ -7,6 +7,8 @@ namespace CP
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Best movies ever!");
         }
     }
 }
