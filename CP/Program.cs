5a42efe55c7e4c1ac8587495defccb169364cfbe
Adam <adaost0110@hotmail.com>
2018-11-02 16:03:56 +0100

    Added copyright statement

--- CP/Program.cs
+++ CP/Program.cs
@@ -9,6 +9,8 @@ namespace CP
             Console.WriteLine("Hello World!");
 
             Console.WriteLine("Best movies ever!");
+
+            Console.WriteLine("Copyright Adam Östberg 2018!");
         }
     }
 }
